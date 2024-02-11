a7fdb8886fd117e98a1e3e39abc6055435e9f598
Mondmarmelade <91614367+Mondmarmelade@users.noreply.github.com>
2024-02-11 17:54:15 +0100

    :sparkles: Updates profile UI screen

--- app/(app)/profile.js
+++ app/(app)/profile.js
@@ -1,56 +1,121 @@
 import { StyleSheet, Text, TouchableOpacity, View, Image } from "react-native";
 import React from "react";
 import { useAuth } from "../../context/AuthContext";
-import { MaterialIcons } from "@expo/vector-icons";
+import { FontAwesome, MaterialIcons } from "@expo/vector-icons";
 
 export default function profile() {
   const { logout, user } = useAuth();
 
-  console.log(user.profilePicture);
+  console.log(user);
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Image
-        style={styles.profilePicture}
-        resizeMode="contain"
-        source={{
-          uri: `${user?.profilePicture}`,
-        }}
-      />
-      <Text>{user?.displayName}</Text>
-      <TouchableOpacity
-        style={styles.button}
-        // disabled={!loginRequest}
-        onPress={() => {
-          logout();
-        }}
-      >
-        <Text style={styles.buttonText}>Abmelden</Text>
-        <MaterialIcons name="logout" size={24} color="white" />
-      </TouchableOpacity>
+      <View style={styles.profileView}>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            paddingBottom: 5,
+          }}
+        >
+          <Image
+            style={styles.profilePicture}
+            resizeMode="contain"
+            source={{
+              uri: `${user?.profilePicture}`,
+            }}
+          />
+          <View style={{ marginLeft: 20 }}>
+            <Text
+              style={{
+                fontSize: 21,
+                fontWeight: "700",
+              }}
+            >
+              {user?.displayName}
+            </Text>
+            <Text
+              selectable={true}
+              style={{
+                fontSize: 16,
+                fontWeight: "300",
+              }}
+            >
+              @{user?.username}
+            </Text>
+            <View style={{ flexDirection: "row", columnGap: 5, marginTop: 5 }}>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => {
+                  logout();
+                }}
+              >
+                <MaterialIcons name="logout" size={18} color="white" />
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.button} onPress={() => {}}>
+                <MaterialIcons name="settings" size={18} color="white" />
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.dataTextView}>
+          <FontAwesome name="road" size={18} />
+          <Text style={styles.dataText}>
+            {new Intl.NumberFormat("de-DE", {
+              style: "unit",
+              unit: "meter",
+            }).format(user?.trainDistance)}
+          </Text>
+        </View>
+
+        <View style={styles.dataTextView}>
+          <FontAwesome name="clock-o" size={21} />
+          <Text style={styles.dataText}>
+            {Math.floor(user?.trainDuration / 60)}h {user?.trainDuration % 60}
+            min
+          </Text>
+        </View>
+
+        <View style={styles.dataTextView}>
+          <FontAwesome name="star-o" size={21} />
+          <Text style={styles.dataText}>{user?.points} Pkt</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  profileView: {
+    width: "90%",
+    height: 200,
+    borderColor: "#E7E7E7",
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 10,
+  },
   profilePicture: {
     width: 100,
     height: 100,
     borderRadius: 10,
   },
   button: {
-    width: "50%",
+    width: 40,
+    height: 30,
     backgroundColor: "#FB2233",
-    height: 50,
-    borderRadius: 10,
+    borderRadius: 5,
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
-    marginTop: 15,
   },
-  buttonText: {
-    fontSize: 16,
-    fontWeight: "800",
-    color: "white",
-    marginRight: 10,
+  dataTextView: {
+    flexDirection: "row",
+    alignItems: "center",
+    columnGap: 5,
+    marginLeft: 5,
+  },
+  dataText: {
+    marginTop: 5,
+    fontSize: 18,
   },
 });
